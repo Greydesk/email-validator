aad0cd2d0a4d25811e80f448ff0001c63e8e59d9
Greydesk <rod.naugler@gmail.com>
2017-03-02 10:57:25 -0400

    Created other test emails and start debugging bad test fails.

--- src/main/java/com/example/email_validator/App.java
+++ src/main/java/com/example/email_validator/App.java
@@ -18,15 +18,23 @@ public class App
     	//Instead of above, check that there is atleast one dot after the @
     	int dot = email.lastIndexOf(".");   //Location of last 
     	int at = email.lastIndexOf("@");    //Location of last @
-    	if (dot-at>0){                      //If the last dot comes after the last @
+    	if (dot-at>0 && at != 0){           //If the last dot comes after the last @
     		passed++;                       //Increment the number of tests passed
+    		System.out.println(email + " - Test 1 passed");
     	}
     	if (email.contains("@")){			//Check to see if the email contains an @ symbol
     		passed++;                       //Increment the number of tests passed
+    		System.out.println(email + " - Test 2 passed");
     	}
     	int otherat = email.indexOf("@");   //Location of first @
-    	if (at == otherat){                 //If at = otherat then there is only one at and thus valid.
-    		passed++;
+    	if (at == otherat && at !=0){       //If at = otherat then there is only one at and thus valid. Ensure not 0
+    		passed++;                       //Increment the number of tests passed
+    		System.out.println(email + " - Test 3 passed");
+    	}
+    	String ePattern = new String("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$");
+    	if (email.matches(ePattern)) {      //The above regular expression should pass on all valid emails.
+    		passed++;                       //Increment passed counter
+    		System.out.println(email + " - Test 4 passed");
     	}
         return passed;
     }
--- src/test/java/com/example/email_validator/AppTest.java
+++ src/test/java/com/example/email_validator/AppTest.java
@@ -31,10 +31,34 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testApp1()
     {
-    	String testEmail = new String("rod.naugler@gmail.com");
+    	String testEmail = new String("rod.naugler@gmail.com"); //This should pass 4 tests
     	int result = App.main(testEmail);
-    	assertEquals( 3,result );
+    	assertEquals( 4,result );
+    }
+    public void testApp2()
+    {
+     	String testEmail = new String("rod@example.com");       //This should pass 4 tests
+    	int result = App.main(testEmail);
+    	assertEquals( 4,result );
+    }
+    public void testApp3()
+    {
+     	String testEmail = new String("rod@com");               //This should pass 2 tests
+    	int result = App.main(testEmail);
+    	assertEquals( 2,result );
+    }
+    public void testApp4()
+    {
+     	String testEmail = new String("rod");                   //This should pass 0 tests
+    	int result = App.main(testEmail);
+    	assertEquals( 0,result );
+    }
+    public void testApp5()
+    {
+     	String testEmail = new String(".com");                  //This should pass 0 tests
+    	int result = App.main(testEmail);
+    	assertEquals( 0,result );
     }
 }
